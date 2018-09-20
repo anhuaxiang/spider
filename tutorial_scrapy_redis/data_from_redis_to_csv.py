9c54610e0a3437cab7e800ada133e4c931530d86
anhauxiang <1342181530@qq.com>
2018-09-21 00:01:20 +0800

    feat: add redis to mysql

--- tutorial_scrapy_redis/data_from_redis_to_csv.py
+++ tutorial_scrapy_redis/data_from_redis_to_csv.py
@@ -1,6 +1,7 @@
 import redis
 import json
 import pymongo
+from DataToMysql import DataToMysql
 
 
 pool = redis.ConnectionPool()
@@ -9,7 +10,7 @@ mongo_cli = pymongo.MongoClient(host='127.0.0.1', port=27017)
 sheet = mongo_cli.hsw.hsw_items
 
 
-def main():
+def main_mongodb():
     offset = 0
     while True:
         # FIFO模式为 blpop，LIFO模式为 brpop，获取键值
@@ -24,6 +25,24 @@ def main():
             print("error processing {}".format(item))
 
 
+def main_mysql():
+    offset = 0
+    dty = DataToMysql(user='root', passwd='', db='hsw')
+    while True:
+        source, data = redis_cli.blpop(['hsw_spider:items'])
+        data = dict(json.loads(data.decode('utf-8')))
+        print(f"processing {data}")
+        dty.write('shw_item', data)
+        offset += 1
+        print(offset)
+        try:
+            pass
+        except KeyError:
+            print(f"error processing {data}")
+
+
 if __name__ == '__main__':
-    main()
+    # main_mongodb()
+    main_mysql()
+
 
