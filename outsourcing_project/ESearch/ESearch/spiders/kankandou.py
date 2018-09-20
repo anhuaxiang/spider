43a6e0e7bce04c43add721604b7e944f9d920b6e
anhauxiang <1342181530@qq.com>
2018-09-21 00:03:43 +0800

    style: reformat coding

--- outsourcing_project/ESearch/ESearch/spiders/kankandou.py
+++ outsourcing_project/ESearch/ESearch/spiders/kankandou.py
@@ -5,8 +5,10 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from ESearch.utils.common import get_md5
 import sys
+
 reload(sys)
 sys.setdefaultencoding("utf-8")
+
 
 class KankandouSpider(CrawlSpider):
     name = 'kankandou'
@@ -14,16 +16,18 @@ class KankandouSpider(CrawlSpider):
     start_urls = ['https://kankandou.com/']
 
     rules = (
-        Rule(LinkExtractor(allow=(r"book/%.*.html"),deny=(r"login.html.*.html",r"reg.html.*.html")),follow=True),
-        Rule(LinkExtractor(allow=(r"book/page/\d+"),deny=(r"login.html.*.html",r"reg.html.*.html")),follow=True),
-        Rule(LinkExtractor(allow=(r"book/view/\d+.html"),deny=(r"login.html.*.html",r"reg.html.*.html")),callback='parse_detail',follow=True),
+        Rule(LinkExtractor(allow=(r"book/%.*.html"), deny=(r"login.html.*.html", r"reg.html.*.html")), follow=True),
+        Rule(LinkExtractor(allow=(r"book/page/\d+"), deny=(r"login.html.*.html", r"reg.html.*.html")), follow=True),
+        Rule(LinkExtractor(allow=(r"book/view/\d+.html"), deny=(r"login.html.*.html", r"reg.html.*.html")),
+             callback='parse_detail', follow=True),
 
     )
+
     def parse_detail(self, response):
-        item_loader = KankandouItemLoader(item=KankandouItem() , response=response)
-        item_loader.add_css("kindle_name",".object-title::text")
+        item_loader = KankandouItemLoader(item=KankandouItem(), response=response)
+        item_loader.add_css("kindle_name", ".object-title::text")
         item_loader.add_xpath("kindle_author", "//*[@class='object-tags']/li[1]/span/descendant-or-self::text()")
-        if len(response.xpath("//*[@class='object-tags']/li[5]/a/@href")) !=0:
+        if len(response.xpath("//*[@class='object-tags']/li[5]/a/@href")) != 0:
             item_loader.add_xpath("kindle_score", "//*[@class='object-tags']/li[5]/a/@href")
         else:
             item_loader.add_value("kindle_score", " ")
